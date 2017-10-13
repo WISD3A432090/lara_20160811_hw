dee7a0728fdf46789923542a8e9cc3067a7b5ae2
M511-50\admin <s3a432090@student.ncut.edu.tw>
2017-10-13 16:56:08 +0800

    每一頁的標題就是頁面名稱

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -79,7 +79,7 @@
 
     <div class="content">
         <div class="title m-b-md">
-            Laravel
+            首頁
         </div>
 
         <div class="links">
--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -79,7 +79,7 @@
 
     <div class="content">
         <div class="title m-b-md">
-            Laravel
+            最新消息
         </div>
 
         <div class="links">
